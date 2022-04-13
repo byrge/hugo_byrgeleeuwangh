ed54c7d9e0f6175d7da16976adca5a95564e39e8
byrge <byrge@leeuwangh.com>
2022-04-13 22:14:04 +0200

    Update to function Set Cookie

--- functions/set_cookie.js
+++ functions/set_cookie.js
@@ -5,19 +5,33 @@ exports.handler = async (event, context) => {
 
   // set custom domain -- remove port number
   const current_domain = ( host_value.match(/:/g) ) ? host_value.slice( 0, host_value.indexOf(":") ) : event.headers.host
-  
-  // create uuid
-  var create_uuid = function () { 
+  console.log('current_domain: ',current_domain)
+
+  // format logging
+  const styles = [
+    'color: black',
+    'background: yellow',
+    'font-size: 30px',
+    'border: 1px solid red',
+    'padding: 10px',
+  ].join(';');
+
+  // function create uuid
+  var create_uuid = function () {
+    console.log('%c%s', styles, 'function create_uuid started');
+
+    //console.log('function create_uuid started');
+
     const ip_encode = new Buffer.from(event.headers['client-ip']);
     const ip_value = ip_encode.toString('base64');
     const seconds_since_epoch = Math.round(Date.now() / 1000)
     return ip_value + '-' + seconds_since_epoch;
   }
-  const uuid = create_uuid();
-  console.log('uuid: ',uuid)
 
-  // create session id
-  var session_id = function () {  
+  // function create session id
+  var session_id = function () {
+    console.log('%c%s', styles, 'function session_id started');
+
     var d = new Date().getTime();
     if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
         d += performance.now();
@@ -28,7 +42,6 @@ exports.handler = async (event, context) => {
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
   }
-  const sessionId = session_id();
 
   ///// cookie header variables
   // set Max-Age to one year
@@ -39,7 +52,7 @@ exports.handler = async (event, context) => {
   let secure;
   let local_host = hostname.indexOf('localhost');
   if(local_host === 0) {
-    secure = ``;
+    secure = `Secure`;
   } else {
     secure = `Secure`;
   }
@@ -54,84 +67,81 @@ exports.handler = async (event, context) => {
   };
 
   ///////////
+  const cookie_header_part = "; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict'";
 
   if(headers_cookies) {
     let cookies = headers_cookies.split(";").reduce(function(obj, str, index) {
       let strParts = str.split("=");
       if (strParts[0] && strParts[1]) { 
-        obj[strParts[0].replace(/\s+/g, '')] = strParts[1].trim(); 
+        obj[strParts[0].replace(/\s+/g, '')] = strParts[1].trim();
       }
       return obj;
     }, {});
-    
-    const uuid = cookies['_uuid'] || create_uuid();
-    const ga = cookies['_ga'];
-    const ga_TSKQWXB2BC = cookies['_ga_TSKQWXB2BC'];
-    const sessionId = cookies['_sessionId'] || session_id();
-    
-    let cookie_list = Object.keys(cookies).map((key) => [{'name':(key), 'value':cookies[key]}]);
 
-    let _ga = cookie_list.filter(f => f[0].name === '_ga');
-    let _ga_TSKQWXB2BC = cookie_list.filter(f => f[0].name === '_ga_TSKQWXB2BC');
-    let _ga_uuid = cookie_list.filter(f => f[0].name === '_uuid');
-    let _ga_sessionId = cookie_list.filter(f => f[0].name === '_sessionId');
-    
-    //
-    let count_uuid = _ga_uuid.length;
-    if(count_uuid === 1) {count_uuid += 10;}
-
-    let count_sessionId = _ga_sessionId.length;
-    if(count_sessionId === 1) {count_sessionId += 20;}
-    
-    let count_ga = _ga.length;
-    if(count_ga === 1) {count_ga += 30;}
-    
-    let supercount = count_uuid + count_sessionId + count_ga
-    
-    /* 
-    ga + sessionId + uuid = 63
-    ga + sessionId = 52
-    ga + uuid = 42
-    sessionId + uuid = 32
-    ga = 31
-    sessionId = 21
-    uuid = 11
-    */
-    let set_multi_value_headers
-    
-    if(supercount > 60) {
-      set_multi_value_headers = {"Set-Cookie": [`_ga=${ga}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`__ga_TSKQWXB2BC=${ga_TSKQWXB2BC}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
-    } else if (supercount > 50) {
-      set_multi_value_headers = {"Set-Cookie": [`_ga=${ga}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_ga_TSKQWXB2BC=${ga_TSKQWXB2BC}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
-    } else if (supercount > 40) {
-      set_multi_value_headers = {"Set-Cookie": [`_ga=${ga}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_ga_TSKQWXB2BC=${ga_TSKQWXB2BC}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`]};
-    } else if (supercount > 30) {
-      set_multi_value_headers = {"Set-Cookie": [`_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`]};
-    } else if (supercount > 20) {
-      set_multi_value_headers = {"Set-Cookie": [`_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
-    } else if (supercount > 10) {
-      set_multi_value_headers = {"Set-Cookie": [`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`]};
-    } else {
-      set_multi_value_headers = {"Set-Cookie": [`_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`]};
+    let cookieHeadersFromReq = [];
+    Object.keys(cookies).forEach(key => {
+      cookieHeadersFromReq.push(key + "=" + cookies[key] + "; Path=/; Domain=localhost; Max-Age=31536000; Secure; SameSite=strict");
+    });
+  
+    var multiValueHeaders = {
+      'Set-Cookie': cookieHeadersFromReq
     }
+    console.log("multiValueHeaders: ", multiValueHeaders)
+
+    
+    let uuid = cookies['_uuid'];
+    if(!uuid) {
+      uuid = create_uuid();
+      cookieHeadersFromReq.push(`_uuid=${uuid}; Path=/; Domain=localhost; Max-Age=31536000; Secure; SameSite=strict`);
+      console.log("uuid created!", uuid)
+    }
+    let sessionId = cookies['_sessionId'];
+    if(!sessionId) {
+      sessionId = session_id();
+      cookieHeadersFromReq.push(`_sessionId=${sessionId}; Path=/; Domain=localhost; Max-Age=31536000; Secure; SameSite=strict`);
+      console.log("sessionId created!", sessionId)
+    }
+
+    // if (cookies['_ga'] && cookies['_ga_TSKQWXB2BC'] && cookies['_gac_UA-55505440-1'] && cookies['_uuid'] && cookies['_sessionId']) {
+    //   // all cookies available
+    //   // set cookies for _ga, _ga_TSKQWXB2BC, _gac_UA-55505440-1, uuid and sessionId
+    //   // console.log('all cookies available from header')
+    //   // console.log('uuid: ', cookies['_uuid'])
+    //   // console.log('sessionId: ', cookies['_sessionId'])
+    //   // console.log('ga: ', cookies['_ga'])
+    //   // console.log('_gac_UA-55505440-1: ', cookies['_gac_UA-55505440-1'])
+    //   // console.log('ga_TSKQWXB2BC: ', cookies['_ga_TSKQWXB2BC'])
+    //   // set_multi_value_headers = {"Set-Cookie": [`_ga=${ga}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_ga_TSKQWXB2BC=${ga_TSKQWXB2BC}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_gac_UA-55505440-1=${gac}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
+    // } 
+    // else if (cookies['_ga'] && cookies['_ga_TSKQWXB2BC'] && cookies['_gac_UA-55505440-1'] && cookies['_uuid'] && cookies['_sessionId']) {
+    //   // console.log('no cookies available from header')
+    //   // set_multi_value_headers = {"Set-Cookie": [`_ga=${ga}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_ga_TSKQWXB2BC=${ga_TSKQWXB2BC}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
+    // }
+    // else if ( !cookies['_uuid'] && !cookies['_sessionId']) {
+    //   //  STEP 4 -- write cookies [sessionId and uuid] first time
+    //   console.log('STEP 4 -- no uuid and sessionId')
+    //   // set_multi_value_headers = {"Set-Cookie": [`_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`,`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`]};    
+    // }
+
     // write cookies
     return {
       statusCode: 200,
       body: '',
-      multiValueHeaders : set_multi_value_headers,
+      multiValueHeaders : multiValueHeaders,
       headers: headers,
     };
+  } else {
+      //// no header cookies available
+      //   so write cookies [sessionId and uuid] first time
+      const uuid = create_uuid();
+      const sessionId = session_id();
+      const set_multi_value_headers = {"Set-Cookie": [`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`, `_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
+
+      return {
+          statusCode: 200,
+          body: '',
+          multiValueHeaders : set_multi_value_headers,
+          headers: headers,
+        };
   }
-
-  //// no header cookies available
-  //   so write cookies [sessionId and uuid] first time
-  const set_multi_value_headers = {"Set-Cookie": [`_sessionId=${sessionId}; Path=/; Domain=${current_domain}; ${secure}; SameSite=strict`, `_uuid=${uuid}; Path=/; Domain=${current_domain}; Max-Age=${maxAge}; ${secure}; SameSite=strict`]};
-
-  return {
-      statusCode: 200,
-      body: '',
-      multiValueHeaders : set_multi_value_headers,
-      headers: headers,
-    };
-
 };
