fa04574c3562eb643433ce049dd18377e00f24f7
Byrge Leeuwangh <byrge@leeuwangh.com>
2022-05-04 17:25:15 +0200

    Changes in mp_analytics

--- functions/mp_analytics.js
+++ functions/mp_analytics.js
@@ -40,29 +40,74 @@ exports.handler = async (event, context) => {
     // user agent
     analyticsRequestBody.append("ua", event.headers["user-agent"])
 
+    // referer
+    let user_referer = event.headers['referer'] || undefined;
+    if(user_referer) {
+        analyticsRequestBody.append("dr", user_referer);
+    }
+
+    // set country
+    let user_country = event.headers['x-country'] || undefined;
+    if(user_country) {
+        analyticsRequestBody.append("geoid", user_country);
+    }
+    // Set user language
+    let user_lang = event.headers['accept-language'];
+    if(user_lang) {
+        user_lang = user_lang.split(',')[0];
+        analyticsRequestBody.append("ul", user_lang)
+    }
+
     // Set event data
     analyticsRequestBody.append("tid", "UA-55505440-1")
     analyticsRequestBody.append("t", "event")
-    analyticsRequestBody.append("ec", "netlify")
-    analyticsRequestBody.append("ea", "function")
-    analyticsRequestBody.append("el", event["path"])
+    analyticsRequestBody.append("ec", "Netlify Function")
+    analyticsRequestBody.append("ea", event["path"])
+    let user_platform = event.headers['sec-ch-ua-platform'];
+    analyticsRequestBody.append("el", user_platform);
 
-    // Set user language
-    let user_lang = event.headers['accept-language'];
-    user_lang = user_lang.split(',')[0];
-    analyticsRequestBody.append("ul", user_lang)
+    // queryStringParameters
+    let searchQuery = event["queryStringParameters"];
+    let params = new URLSearchParams(searchQuery)
+    // get Marketing (utm) parameters
+    let param_gclid = params.get('gclid') || undefined;
+    let param_utmSource = params.get('utm_source') || undefined;
+    let param_utmMedium = params.get('utm_medium') || undefined;
+    let param_utmCampaign = params.get('utm_campaign') || undefined;
 
-    console.log('queryStringParameters >>>> ', event["queryStringParameters"]);
-    
-
-    //for (const paramName in body.params) {
-    //  analyticsRequestBody.append(paramName, body.params[paramName])
-    //}
+    if(param_utmSource && param_utmMedium) {
+        console.log('set utm params')
+        analyticsRequestBody.append("cs", param_utmSource);
+        analyticsRequestBody.append("cm", param_utmMedium);
+    }
+    if(param_utmCampaign) {
+        analyticsRequestBody.append("cn", param_utmCampaign);
+    }
+    if(param_gclid) {
+        console.log('set gclid params')
+        analyticsRequestBody.append("cs", 'google');
+        analyticsRequestBody.append("cm", 'cpc');
+        analyticsRequestBody.append("gclid", param_gclid);
+    }
+    let google_organic = undefined;
+    if(user_referer) {
+        google_organic = user_referer.includes('google');
+    }
+    if(!param_gclid && google_organic) {
+        console.log('set organic params')
+        analyticsRequestBody.append("cs", 'google');
+        analyticsRequestBody.append("cm", 'organic');
+    }
+    else if(!param_utmSource && !param_gclid) {
+        console.log('no params > direct')
+        analyticsRequestBody.append("cs", '(direct)');
+        analyticsRequestBody.append("cm", '(none)');
+    }
 
     // Send event to Google Analytics
     let ga_url = 'https://www.google-analytics.com/collect?';
     let gtm_server_url = 'https://tagging.byrgeleeuwangh.com/j/collect?';
-    console.log('mp - analyticsRequestBody: ', gtm_server_url+analyticsRequestBody.toString())
+    console.log('mp - analyticsRequestBody: ', ga_url+analyticsRequestBody.toString())
 
     await axios({
         method: 'post',
